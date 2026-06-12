33738798c40e058cee042f1ffaa7fae1c8ba2d3c
nuwin-vinwath-zero47-mobios-lk <nuwin.vinwath@mobios.lk>
2026-06-12 16:41:04 +0530

    Add initial project structure with Maven wrapper and application properties.

---
+++ patient_request/src/main/java/com/v1/patient_request/PatientRequestApplication.java
@@ -0,0 +1,13 @@
+package com.v1.patient_request;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientRequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientRequestApplication.class, args);
+	}
+
+}
---
+++ patient_request/src/test/java/com/v1/patient_request/PatientRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v1.patient_request;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientRequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
